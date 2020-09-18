8efd922dac1961dce7b2be43b1deca85fadb7f3b
Tucker Leach <leachtucker@gmail.com>
2020-09-18 10:52:01 -0700

    State and helper functions set for form. Laying out elements

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import axios from 'axios';
 import "./App.css";
 
 // Components
@@ -7,7 +8,7 @@ import PizzaForm from './Components/PizzaForm';
 import FoodDelivery from "./Components/FoodDelivery";
 
 // Initial State
-const intialFormValues = {
+const initialFormValues = {
   size: "",
   sauce: "",
   specialInstructions: "",
@@ -33,11 +34,57 @@ const initialFormErrors = {
 
 const initialOrders = [];
 
+const initialDisabled = true;
+
 
 
 const App = () => {
   const [orders, setOrders] = useState(initialOrders);
-  const [formValues, setFormValues] = useState(intialFormValues);
+  const [formValues, setFormValues] = useState(initialFormValues);
+  const [formErrors, setFormErrors] = useState(initialFormErrors);
+  const [disabled, setDisabled] = useState(initialDisabled);
+
+  // HELPERS //
+
+  // Post new order to reqres with axios
+  // add new order to state upon success of post
+  // Console.log the reponse data
+  const postNewOrder = newOrder => {
+    axios.post('https://reqres.in/api/users', newOrder)
+      .then(response => {
+        setOrders([...orders, response.data]);
+        // Logging the the response data
+        console.log(response.data);
+        setFormValues(initialFormValues);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }
+
+  // EVENT HANDLERS //
+  const inputChange = (name, value) => {
+    setFormValues({ ...formValues, [name]: value });
+  }
+
+  const formSubmit = () => {
+    const newOrder = {
+      size: formValues.size,
+      sauce: formValues.sauce,
+      specialInstructions: formValues.specialInstructions.trim(),
+      glutenFree: formValues.glutenFree,
+      pepporoni: formValues.pepporoni,
+      sausage: formValues.sausage,
+      onions: formValues.onions,
+      dicedTomatoes: formValues.dicedTomatoes
+    }
+  }
+
+  // SIDE EFFECTS //
+  useEffect(() => {
+    // Validate form values each time they change
+
+  }, [formValues])
 
   return (
     <>
@@ -50,7 +97,7 @@ const App = () => {
         </div>
       </header>
       <Route exact path="/" component={() => <FoodDelivery />} />
-      <Route path="/pizza" component={() => <PizzaForm />} />
+      <Route path="/pizza" component={() => <PizzaForm values={formValues} errors={formErrors} disabled={disabled} change={inputChange} submit={formSubmit} />} />
     </>
   );
 };
--- src/Components/PizzaForm.js
+++ src/Components/PizzaForm.js
@@ -1,11 +1,84 @@
 import React from 'react';
 
-export default function() {
+export default function(props) {
+    const { values, errors, disabled,  change, submit} = props;
 
+    const onSubmit = evt => {
+        evt.preventDefault();
+        submit();
+    }
+
+    const onChange = evt => {
+        const { name, value, type, checked } = evt.target;
+        const valueToUse = type === 'checkbox' ? checked : value;
+        change(name, valueToUse);
+    }
 
     return (
-        <div>
-            <p>Make your pizza</p>
-        </div>
+        <section className="pizza-maker">
+            <div className="heading-container">
+                <h3>Build Your Own Pizza</h3>
+            </div>
+            <div className="hero"></div>
+            <div className="form-container">
+                <h3>Build Your Own Pizza</h3>
+                <form>
+                    <div className="form-group">
+                        <div className="form-bar">
+                            <h4>Choice of Size:</h4>
+                            <span>Required</span>
+                        </div>
+                        <label>
+                            <select value={values.size} name="size" onChange={onChange}>
+                                <option value="">--Select an option--</option>
+                                <option value="small">Small</option>
+                                <option value="medium">Medium</option>
+                                <option value="large">Large</option>
+                                <option value="extra large">Extra Large</option>
+                            </select>
+                        </label>
+                    </div>
+
+                    <div className="form-group">
+                        <div className="form-bar">
+                            <h4>Choice of Sauce:</h4>
+                            <span>Required</span>
+                        </div>
+                        <label>
+                            <input type="radio" name="sauce" value="original red" checked={values.sauce === 'original red'} onChange={onChange} /> Original Red
+                        </label>
+                        <label>
+                            <input type="radio" name="sauce" value="garlic ranch" checked={values.sauce === 'garlic ranch'} onChange={onChange} /> Garlic Ranch
+                        </label>
+                        <label>
+                            <input type="radio" name="sauce" value="bbq sauce" checked={values.sauce === 'bbq sauce'} onChange={onChange} /> BBQ Sauce
+                        </label>
+                        <label>
+                            <input type="radio" name="sauce" value="spinach alfredo" checked={values.sauce === 'spinach alfredo'} onChange={onChange} /> Spinach Alfredo
+                        </label>
+                    </div>
+
+                    <div className="form-group">
+                        <div className="form-bar">
+                            <h4>Add Toppings:</h4>
+                            <span>Choose up to 4.</span>
+                        </div>
+                        <label>
+                            <input type="checkbox" name="sauce" value="original red" checked={values.sauce === 'original red'} onChange={onChange} /> Original Red
+                        </label>
+                        <label>
+                            <input type="checkbox" name="sauce" value="garlic ranch" checked={values.sauce === 'garlic ranch'} onChange={onChange} /> Garlic Ranch
+                        </label>
+                        <label>
+                            <input type="checkbox" name="sauce" value="bbq sauce" checked={values.sauce === 'bbq sauce'} onChange={onChange} /> BBQ Sauce
+                        </label>
+                        <label>
+                            <input type="checkbox" name="sauce" value="spinach alfredo" checked={values.sauce === 'spinach alfredo'} onChange={onChange} /> Spinach Alfredo
+                        </label>
+                    </div>
+
+                </form>
+            </div>
+        </section>
     );
 }
