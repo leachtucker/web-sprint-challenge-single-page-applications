1d1ab0fb2b06b3a13df1a025142cea2af9f1d5be
Tucker Leach <leachtucker@gmail.com>
2020-09-18 09:48:48 -0700

    Completed home page/food delivery component with styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import "./App.css";
+
+// Components
+import PizzaForm from './Components/PizzaForm';
+import FoodDelivery from "./Components/FoodDelivery";
+
 
 const App = () => {
+
   return (
     <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+      <header>
+        <div className="nav-bar">
+          <h1>Lambda Eats</h1>
+          <nav>
+            <Link to="/">Home</Link>
+          </nav>
+        </div>
+      </header>
+      <Route exact path="/" component={() => <FoodDelivery />} />
+      <Route path="/pizza" component={() => <PizzaForm />} />
     </>
   );
 };
+
 export default App;
---
+++ src/Components/PizzaForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function() {
+
+
+    return (
+        <div>
+            <p>Make your pizza</p>
+        </div>
+    );
+}
